fb106f147f3cec2c5741607f2257698779e47016
Shubham <princeshubham22699@gmail.com>
2020-02-18 20:54:15 +0530

    Map Marker's Automation Added with Modal Layout for showing respective Location Picture and Info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker  } from 'google-maps-react';
 import Header from './Component/Header';
 import Footer from './Component/Footer';
+import MapContainer from './Component/MapContainer';
 import './App.css';
 
 const mapStyles = {
@@ -13,7 +14,7 @@ const mapStyles = {
 
 const api_key = 'AIzaSyDVwHvJvyD5eWyMn30Sk67kQpLNJXd1WMU';
 
-export class MapContainer extends Component {
+export class App extends Component {
   
   state = {
     showingInfoWindow: false,  //Hides or the shows the infoWindow
@@ -21,35 +22,11 @@ export class MapContainer extends Component {
     selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
   };
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
-    }
-  };
-
   render() {
     return (
         <React.Fragment>
           <Header />  
-          <Map
-            google={this.props.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={{
-              lat: -1.2884,
-              lng: 36.8233
-            }}
-          />
+          <MapContainer />
         </React.Fragment>
     );
   }
@@ -57,4 +34,4 @@ export class MapContainer extends Component {
 
 export default GoogleApiWrapper({
   apiKey: api_key
-})(MapContainer);
+})(App);
--- src/Component/MapContainer.js
+++ src/Component/MapContainer.js
@@ -1,27 +1,103 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component, Fragment } from 'react';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+  Button, Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label } from 'reactstrap';
+import { Map, GoogleApiWrapper, InfoWindow, Marker  } from 'google-maps-react';
 
 const mapStyles = {
-  width: '100%',
-  height: '100%'
+  width: '80%',
+  height: '90%',
+  marginLeft: 'auto',
+  marginRight: 'auto'
 };
 
+const api_key = 'AIzaSyDVwHvJvyD5eWyMn30Sk67kQpLNJXd1WMU';
+
 export class MapContainer extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
+        {latitude: 47.359423, longitude: -122.021071},
+        {latitude: 47.2052192687988, longitude: -121.988426208496},
+        {latitude: 47.6307081, longitude: -122.1434325},
+        {latitude: 47.3084488, longitude: -122.2140121},
+        {latitude: 47.5524695, longitude: -122.0425407}],
+      showingInfoWindow: false,  //Hides or the shows the infoWindow
+      activeMarker: {},          //Shows the active marker upon click
+      selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+    };
+
+    this.displayMarkers = this.displayMarkers.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return <Marker key={index} id={index} position={{
+       lat: store.latitude,
+       lng: store.longitude
+     }}
+     onClick={this.toggleModal} />
+    })
+  }
+
+  toggleModal() {
+    this.setState({
+        isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value + " Remember Me: " + this.remember.checked);
+    event.preventDefault();
+  }
+
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
+      <React.Fragment>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        >
+          {this.displayMarkers()}
+        </Map>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Bank Condition</ModalHeader>
+            <ModalBody>
+              Some Picture and Info is Gonna Be Here !!!
+              Wait Patiently ...
+            </ModalBody>
+        </Modal>    
+      </React.Fragment>  
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDVwHvJvyD5eWyMn30Sk67kQpLNJXd1WMU'
+  apiKey: api_key
 })(MapContainer);
